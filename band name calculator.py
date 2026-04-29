37ba23b702e9d90b3d52e8cf42e866b6e3df2ea3
vivanP-9062 <vivanpatel9062@gmail.com>
2026-04-30 04:31:09 +0530

    Add files via upload
    my first python project

---
+++ band name calculator.py
@@ -0,0 +1,4 @@
+print("give the following answers for a band name")
+city = input("what city did you grow up in?\n")
+pet = input("what is the name of your pet?\n")
+print("your band name could be: " + city + " " + pet) 
